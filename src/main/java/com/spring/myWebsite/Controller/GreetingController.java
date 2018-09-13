e1b04146d7470d22fbd9471a399ff66492cea912
LewisWilliams <lewiswilliams1501@gmail.com>
2018-09-13 18:41:26 +0100

    Fixed Get+Post

--- src/main/java/com/spring/myWebsite/Controller/GreetingController.java
+++ src/main/java/com/spring/myWebsite/Controller/GreetingController.java
@@ -1,18 +1,36 @@
 package com.spring.myWebsite.Controller;
 
+import com.spring.myWebsite.Model.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/postName")
+    public String addCustomer(Model model){
+        /*var customer = new Customer();
+        customer.setFirstName(fname);
+        customer.setLastName(lname);
+
+        model.addAttribute("firstName",customer.getFirstName());
+        model.addAttribute("lastName",customer.getLastName());*/
+
+        model.addAttribute("customer",new Customer());
+
+        return "customer";
+    }
+
+    @PostMapping("/postName")
+    public String showcustomer(@ModelAttribute Customer customer){
+        return "customerResult";
     }
 
 }
